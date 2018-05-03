7b9eab0e725fc4baf2bf08ae86373bd0b9304341
VincentSch4rf <s166149@hft-leipzig.de>
2018-05-03 14:08:30 +0200

    Revised nlms_diff_pre and nlms_predecessor
    Now using a[j,n] correctly

--- octave/nlms_diff_pre.m
+++ octave/nlms_diff_pre.m
@@ -5,21 +5,29 @@ function [y, e, a] = nlms_diff_pre(x, u, M)
   for k = 2:N
     if M < k-2
       m = M;
-    else
-      m = k-2;
+    else		#Solange Anzahl(Werte betrachtet) < M ..
+      m = k-2;	#.. muss M angepasst werden sonst: IndexOutOfBounds
     end
-    y(k) = x(k-1);
-    for j = 1:m
-      y(k) += a(k)*(x(k-1)-x(k-j-1));
+    y(k) = x(k-1); #Setze y[n] auf Vorgänger des aktuell betrachteten Signals
+    for j = 1:m #Summe von j=1 bis M..
+	  # .. zu y[n] den Signalwert x[n-1] abzueglich des relativen Vorängers x[n-j-1]
+	  # addieren und mit dem Koeffizienten a(j,k) gewichten
+      y(k) += a(k)*(x(k - 1) - x(k - j - 1));
     end
-    e(k) = x(k) - y(k);
-    absxn = 0;
-    for j = 1:m
-      absxn += (x(k-j)-x(k-j-1)).^2;
+    e(k) = x(k) - y(k); #Fehler als Different aus x[n] und y[n]
+    absxn = 0; #||x[n]||^2 = ..
+    for j = 1:m # .. Summe von j=1 bis M ..
+	  #Differenzieller Bezug auf Vorgänger durch x[n-j] statt x[n-1]
+      absxn += (x(k - j) - x(k - j - 1)).^2;
     end
-    if absxn == 0 absxn = 1; end
-    for j = 1:m
-      a(k+1) = a(k) + u*e(k)*((x(k-j)-x(k-j-1))/absxn);
+	#Filterkoeffizienten nachfuehren
+	for j = 1:m
+		if absxn == 0 
+			a(j,k + 1) = a(j,k); #Division furch 0 vermeiden
+		else
+			#Differenzieller Bezug auf Vorgänger durch x[n-j] statt x[n-1]
+			a(k+1) = a(k) + u * e(k) * ((x(k - j) - x(k - j - 1)) / absxn);
+		end
     end
   end
 end
--- octave/nlms_predecessor.m
+++ octave/nlms_predecessor.m
@@ -3,23 +3,29 @@ function [y, e, a] = nlms_predecessor(x, u, M)
   a = zeros(1,N);
   y = zeros(1,N);
   for k = 2:N
-    if M < k-2
+    if M < k - 2
       m = M;
-    else
-      m = k-2;
+    else		 #Solange Anzahl(Werte betrachtet) < M ..
+      m = k - 2; #.. muss M angepasst werden sonst: IndexOutOfBounds
     end
-    y(k) = x(k-1);
-    for j = 1:m
-      y(k) += a(k)*(x(k-1)-x(k-j-1));
+    y(k) = x(k - 1); # Setze y[n] auf Vorgänger des aktuell betrachteten Signals
+    for j = 1:m #Summe von j=1 bis M..
+	  # .. zu y[n] den Signalwert x[n-1] abzueglich des relativen Vorängers x[n-j-1]
+	  # addieren und mit dem Koeffizienten a(j,k) gewichten
+      y(k) += a(j,k) * (x(k - 1) - x(k - j - 1));
     end
-    e(k) = x(k) - y(k);
-    absxn = 0;
-    for j = 1:m
-      absxn += (x(k-1)-x(k-j-1)).^2;
+    e(k) = x(k) - y(k); #Fehler als Different aus x[n] und y[n]
+    absxn = 0; #||x[n]||^2 = ..
+    for j = 1:m #.. Summe von j=1 bis M ..
+      absxn += (x(k - 1) - x(k - j - 1)).^2;
     end
-    if absxn == 0 absxn = 1; end
-    for j = 1:m
-      a(k+1) = a(k) + u*e(k)*((x(k-1)-x(k-j-1))/absxn);
-    end
+	#Filterkoeffizienten nachfuehren
+	for j = 1:m
+		if absxn == 0 
+			a(j,k+1) = a(j,k); #Division furch 0 vermeiden
+		else
+			a(j,k+1) = a(j,k) + u * e(k) * ((x(k - 1) - x (k - j - 1)) / absxn);
+		end
+	end
   end
 end
